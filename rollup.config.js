c183491844be0198e446ac1d8f1abe2672ec74ee
Abhishek Bansal <abhishekbansal1993@gmail.com>
2017-12-27 16:04:12 +0530

    Updated build steps in rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,6 @@
+import uglify from 'rollup-plugin-uglify';
+import { minify } from 'uglify-es';
+import babel from 'rollup-plugin-babel';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import json from 'rollup-plugin-json';
@@ -22,9 +25,23 @@ export default {
                 // crypto: 'crypto',
                 // global: 'window'
             }
-        })
+        }),
+        babel({
+            exclude: 'node_modules/**',
+        }),
+        uglify({
+            output: {
+                comments: function(node, comment) {
+                    var text = comment.value;
+                    if (text == ' Copyright (c) 2017 VadR Network Pvt. Ltd. ')
+                        return true;
+                    else
+                        return false;
+                }
+            }
+        }, minify)
     ],
     globals: {
     },
-    banner: '/* Copyright (c) 2017 VadR Network Pvt. Ltd. */',
+    banner: '/* Copyright (c) 2017 VadR Network Inc. */',
 };
